b871c7c11e4694ac902c87ff53adbafd71491713
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2025-05-25 21:02:47 -0700

    Commit reproducible error

---
+++ spec/support/type-check.cjs
@@ -0,0 +1,15 @@
+require('ts-node').register({
+  project: "tsconfig.json",
+  transpileOnly: true,
+  files: true
+})
+
+// import { register } from 'ts-node'
+
+// register({
+//   project: "tsconfig.json",
+//   transpileOnly: true,
+//   files: true,
+//   esm: true,
+// })
+
--- spec/support/type-check.js
+++
@@ -1,6 +0,0 @@
-require('ts-node').register({
-  project: "tsconfig.json",
-  transpileOnly: true,
-  files: true
-})
-
