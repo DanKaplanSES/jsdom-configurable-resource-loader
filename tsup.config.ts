542f9634458aca699f8b043ee20d28fcc27f7c77
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2025-05-25 23:41:46 -0700

    Bump version and rename js/d file extensions

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  outExtension: (ctx)=> {
+    return {
+        js: ctx.format === "esm" ? ".mjs" : ".cjs",
+        dts: ctx.format === "esm" ? ".d.mts" : ".d.cts"
+    }
+  },
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: "inline",
